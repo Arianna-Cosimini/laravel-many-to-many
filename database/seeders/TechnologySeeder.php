817979c1d3a386a78443db5b56641a7736a89941
arianna-cosimini <arianna.cosimini@gmail.com>
2024-04-24 15:50:48 +0200

    Creato e popolato seeder technology

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies=[
+            "Html",
+            "Css",
+            "Javascript",
+            "Vue.js",
+            "Php",
+            "Laravel",
+        ];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+
+            $newTechnology->technology = $technology;
+            $newTechnology->save();
+        }
     }
 }
