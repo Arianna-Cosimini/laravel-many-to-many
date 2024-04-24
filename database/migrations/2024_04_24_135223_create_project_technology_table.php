b6382b6adb35973405f9ad35d60522daab45e5d8
arianna-cosimini <arianna.cosimini@gmail.com>
2024-04-24 15:56:53 +0200

    nnella migrazione della tabella ponte ho assegnato le chiavi a project e technology. Fatta migrazione

--- database/migrations/2024_04_24_135223_create_project_technology_table.php
+++ database/migrations/2024_04_24_135223_create_project_technology_table.php
@@ -12,8 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('project_technology', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            // aggiungo il campo project_id e gli dico che deve essere una chiave esterna con vincolo
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            // il metodo cascadeOnDelete elimina la riga di questa tabella ponte in caso la riga a cui fa riferimento tramite la sua chiave esterna
+            // venga cancellata dalla tabella collegata
+
+            $table->foreignId('technology_id')->constrained()->cascadeOnDelete();
+
+            // per convenzione si indica comunque che il database deve considerare come chiave primaria
+            // la coppia dei due valori
+            $table->primary(['project_id', 'technology_id']);
         });
     }
 
