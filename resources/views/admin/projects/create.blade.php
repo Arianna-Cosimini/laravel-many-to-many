32a4c5b7fd69d903b4c05fa5bf042874b1a2af0d
arianna-cosimini <arianna.cosimini@gmail.com>
2024-04-24 16:10:52 +0200

    In create.blade aggiunto foreach per stampare in pagina le tecnologie

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -90,6 +90,22 @@
 
             </div>
 
+            <div class="mb-4">
+                <label class="mb-2" for="">technology</label>
+                <div class="d-flex gap-4">
+
+                    @foreach ($technologies as $technology)
+                        <div class="form-check ">
+
+                            <input type="checkbox" name="technologies[]" value="{{ $technology->id }}" class="form-check-input"
+                                id="technology-{{ $technology->id }}" {{ in_array($technology->id, old('technologies', [])) ? 'checked' : '' }}>
+
+                            <label for="technology-{{ $technology->id }}" class="form-check-label">{{ $technology->technology }}</label>
+                        </div>
+                    @endforeach
+
+                </div>
+            </div>
             <button class="btn btn-primary">Aggiungi</button>
 
         </form>
